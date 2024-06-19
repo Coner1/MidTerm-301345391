cb7910f07dd58b23676d39d814c5d555c1300458
wangkang <wangkang1102@hotmail.com>
2024-06-18 20:00:34 -0400

    add product

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./AddProduct.css";
+
+const AddProduct = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+    category: "",
+    quantity: "",
+    price: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(formData, null, 2));
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add Product</h1>
+      <input
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+      <input
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="Description"
+      />
+      <input
+        name="category"
+        value={formData.category}
+        onChange={handleChange}
+        placeholder="Category"
+      />
+      <input
+        name="quantity"
+        value={formData.quantity}
+        onChange={handleChange}
+        placeholder="Quantity"
+      />
+      <input
+        name="price"
+        value={formData.price}
+        onChange={handleChange}
+        placeholder="Price"
+      />
+      <div>
+        <button type="submit">Submit</button>
+        <button type="reset">Cancel</button>
+      </div>
+    </form>
+  );
+};
+
+export default AddProduct;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AddProduct from "./components/AddProduct.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AddProduct />
+  </React.StrictMode>
+);
